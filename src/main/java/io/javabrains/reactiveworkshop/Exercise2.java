948609909c9f4ea8c946e7a864cfcd9e43ef22ad
utsavcoding <utsavtiwary10@gmail.com>
2024-05-10 21:18:34 +0530

    EX-2 Completed reading from flux reactive stream and print

--- src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
@@ -9,10 +9,12 @@ public class Exercise2 {
         // Use ReactiveSources.intNumbersFlux() and ReactiveSources.userFlux()
 
         // Print all numbers in the ReactiveSources.intNumbersFlux stream
-        // TODO: Write code here
+        // StreamSources was synchronous and the elements used to be printed with one after another till the stream was finished
+        // Here, whenever the event happens, we are going to print the event/element. This is completely random. So, we have kept a input at the end for delay
+        ReactiveSources.intNumbersFlux().subscribe(e -> System.out.println(e));
 
         // Print all users in the ReactiveSources.userFlux stream
-        // TODO: Write code here
+        ReactiveSources.userFlux().subscribe(user -> System.out.println(user));
 
         System.out.println("Press a key to end");
         System.in.read();
