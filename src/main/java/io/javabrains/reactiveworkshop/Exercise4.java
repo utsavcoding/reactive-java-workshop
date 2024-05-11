733af66768695865b9c6e92cd4c9577f21b31455
utsavcoding <utsavtiwary10@gmail.com>
2024-05-11 13:25:04 +0530

    EX-4,5: Different possible outcomes of subscribe and method signature with backpressure impl in flux using basesubscriber

--- src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
@@ -1,6 +1,7 @@
 package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class Exercise4 {
 
@@ -9,10 +10,16 @@ public class Exercise4 {
         // Use ReactiveSources.intNumberMono()
 
         // Print the value from intNumberMono when it emits
-        // TODO: Write code here
+        Integer value = ReactiveSources.intNumberMono().block();
+        ReactiveSources.intNumberMono().subscribe(
+                number -> System.out.println(number), // in case of event is fired
+                err -> System.out.println(err.getMessage()), //in case we have an error
+                () -> System.out.println("Complete") //on complete event
+        );
 
         // Get the value from the Mono into an integer variable
-        // TODO: Write code here
+        Optional<User> user = ReactiveSources.userMono().blockOptional();
+
 
         System.out.println("Press a key to end");
         System.in.read();
--- src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
@@ -1,5 +1,8 @@
 package io.javabrains.reactiveworkshop;
 
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+
 import java.io.IOException;
 
 public class Exercise5 {
@@ -9,13 +12,32 @@ public class Exercise5 {
         // Use ReactiveSources.intNumberMono() and ReactiveSources.userMono()
 
         // Subscribe to a flux using the error and completion hooks
-        // TODO: Write code here
+        /*ReactiveSources.intNumbersFlux().subscribe(
+                number -> System.out.println(number),
+                err -> System.out.println(err.getMessage()),
+                () -> System.out.println("Complete")
+        );
+         */
 
         // Subscribe to a flux using an implementation of BaseSubscriber
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux().subscribe(new MySubscriber<>());
 
         System.out.println("Press a key to end");
         System.in.read();
+    }
+
+}
+
+class MySubscriber<T> extends BaseSubscriber<T> {
+
+    public void hookOnSubscribe(Subscription subscription) {
+        System.out.println("Subscribe happened");
+        request(2);//give me 2 items whenever it is ready
+    }
+
+    public void hookOnNext(T value) {
+        System.out.println(value.toString() + " received");
+        request(1);//give me 1 more whenever you are ready
     }
 
 }
