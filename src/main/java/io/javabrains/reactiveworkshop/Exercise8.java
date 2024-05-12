052b4e9dd2e70eb7179d6805bef2e447f63bdf71
utsavcoding <utsavtiwary10@gmail.com>
2024-05-12 13:25:53 +0530

    EX-8,9 More operators and advanced operations

--- src/main/java/io/javabrains/reactiveworkshop/Exercise8.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise8.java
@@ -12,14 +12,23 @@ public class Exercise8 {
         // Use ReactiveSources.intNumbersFluxWithException()
 
         // Print values from intNumbersFluxWithException and print a message when error happens
-        // TODO: Write code here
+        ReactiveSources.intNumbersFluxWithException()
+                .subscribe(x -> System.out.println(x),
+                        err -> System.out.println(err.getMessage())); //incase we want to suppress the error
+
+        ReactiveSources.intNumbersFluxWithException()
+                .doOnError(err -> System.out.println("Error!!" + err.getMessage()))
+                .subscribe(e -> System.out.println(e)); // error will reach subscribe
 
         // Print values from intNumbersFluxWithException and continue on errors
-        // TODO: Write code here
-
+        ReactiveSources.intNumbersFluxWithException()
+                .onErrorContinue((e, item) -> System.out.println("Error!!" + e.getMessage() + item))
+                .subscribe(e -> System.out.println(e));
         // Print values from intNumbersFluxWithException and when errors
         // happen, replace with a fallback sequence of -1 and -2
-        // TODO: Write code here
+        ReactiveSources.intNumbersFluxWithException()
+                .onErrorResume(err -> Flux.just(-1, -2))
+                .subscribe(num -> System.out.println(num));
 
         System.out.println("Press a key to end");
         System.in.read();
--- src/main/java/io/javabrains/reactiveworkshop/Exercise9.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise9.java
@@ -10,13 +10,14 @@ public class Exercise9 {
         // Use ReactiveSources.intNumbersFlux()
 
         // Print size of intNumbersFlux after the last item returns
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux().count().subscribe(e -> System.out.println(e));
 
         // Collect all items of intNumbersFlux into a single list and print it
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux().collectList().subscribe(e -> System.out.println(e));
 
         // Transform to a sequence of sums of adjacent two numbers
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux().buffer(2).map(list -> list.get(0) + list.get(1))
+                .subscribe(sum -> System.out.println(sum));
 
         System.out.println("Press a key to end");
         System.in.read();
